12c2c813c0bd5c3af6dc59676f77a6f0a0764774
Leonel Candido da Silva <leonel.da.silva@capgemini.com>
2019-11-09 16:58:07 +0100

    new test

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,7 +1,12 @@
 from .context import factorial
 import pytest
 
-def test_factorial():
+def test_factorial_5():
     actual = factorial.factorial(5)
     expected = 120
+    assert expected == actual
+
+def test_factorial_10():
+    actual = factorial.factorial(10)
+    expected = 3628800
     assert expected == actual
