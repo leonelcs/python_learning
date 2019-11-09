a2710cad8ad25b238cdb405ab1b4a282791f8a53
Leonel Candido da Silva <leonel.da.silva@capgemini.com>
2019-11-09 17:06:03 +0100

    test with exceptions raises

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -10,3 +10,8 @@ def test_factorial_10():
     actual = factorial.factorial(10)
     expected = 3628800
     assert expected == actual
+
+@pytest.mark.xfail(raises=TypeError)
+def test_wrong_type_factorial():
+    actual = factorial.factorial("4")
+    assert actual == 24
